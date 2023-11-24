96e40648a6c6f4026397b5221548ac89a57a7287
Pooja <pooja3@pdx.edu>
2023-11-24 10:21:18 -0800

    added styling for the login form

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,15 +1,15 @@
 use yew::prelude::*;
 
-
+use crate::components::input::*;
 #[function_component(Login)]
 pub fn login() -> Html {
     html! {
-        <form>
-            <label>{"Username"}</label>
-            <input type="text" />
-            <label>{"Password"}</label>
-            <input type="text" />
-            <button type="submit">{"Login"}</button>
-        </form>
+        <div class="form-container">
+            <form class="login-form">
+                <Input input_type="text" name="username" label="Username"/>
+                <Input input_type="password" name="password" label="Password" />
+                <button class="form-button" type="submit">{"Login"}</button>
+            </form>
+        </div>
     }
 }
