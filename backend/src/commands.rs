28b474ac4ee01a5d68a61eb6e604854c563164c1
Pooja <pooja3@pdx.edu>
2023-11-27 17:47:22 -0800

    tried password-hashing

--- backend/src/commands.rs
+++ backend/src/commands.rs
@@ -1,3 +1,6 @@
+use argon2::PasswordHasher;
+use argon2::password_hash::SaltString;
+use argon2::password_hash::rand_core::OsRng;
 use diesel_async::{AsyncPgConnection,AsyncConnection};
 use crate::models::NewUser;
 use crate::repositories::{UserRepository, RoleRepository};
@@ -11,7 +14,10 @@ async fn load_db_connection() -> AsyncPgConnection {
 pub async fn create_user(username: String, password: String, role_codes: Vec<String>){
     let mut c = load_db_connection().await;
 
-    let new_user = NewUser {username, password};
+    let salt = SaltString::generate(OsRng);
+    let argon2 = argon2::Argon2::default();
+    let password_hash = argon2.hash_password(password.as_bytes(),&salt).unwrap();
+    let new_user = NewUser {username, password: password_hash.to_string()};
     let user = UserRepository::create(&mut c, new_user, role_codes).await.unwrap();
     println!("User created {:?}", user);
 
--- backend/src/rocket_routes/mod.rs
+++ backend/src/rocket_routes/mod.rs
@@ -1,5 +1,16 @@
+use std::error::Error;
+use rocket::http::Status;
+use rocket::response::status::Custom;
+use rocket::serde::json::{json, Value};
+
 pub mod rustaceans;
+pub mod authorization;
 
 #[derive(rocket_db_pools::Database)]
 #[database("postgres")]
 pub struct DbConn(rocket_db_pools::diesel::PgPool);
+
+pub fn server_error(e: Box<dyn Error>) -> Custom<Value> {
+    rocket::error!("{}", e);
+    Custom(Status::InternalServerError, json!("Error"))
+}
